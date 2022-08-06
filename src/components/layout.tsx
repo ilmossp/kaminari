2557ba86cf0396ddf85186726173aedf35af70e4
ILMOSS <medilyasselmoudene@gmail.com>
2022-08-06 15:55:26 +0100

    fixed layouts

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "./header";
 import BottomNavbar from "../components/bottom-navbar";
-import SearchBar from "../components/search-bar";
 
 
 type LayoutProps={child:JSX.Element,routeName:string}
@@ -10,7 +9,6 @@ function Layout({child,routeName}:LayoutProps) {
   return (
     <div className="w-full relative">
       <Header routeName={routeName} />
-      <SearchBar />
       <main>{child}</main>
       <BottomNavbar />
     </div>
